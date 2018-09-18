a99bbf0626a8314fff2153465f3fc935b0f57b68
Steven <asdf@Yahoo.com>
2018-09-18 11:40:53 -0400

    Add project page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  })
+});
+
 app.get('/about', (req, res) => {
   console.log(res);
   res.render('about.hbs', {
