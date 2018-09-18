1e768b0b6a2b6c4fd89560372756c77a965a34df
Steven <asdf@Yahoo.com>
2018-09-18 11:03:22 -0400

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+// port is used for Heroku since its port is always changing we need to access it through process.env.PORT
+// process.env is an object that stores all our environment variables as key value pairs
+const port = process.env.PORT || 3000;
 var app = express();
 
-
+// app.set takes two arguments, key you want to set, value is what you want to use.. eg: view engine we want to use is 'hbs'.
 app.set('view engine', 'hbs');
-
+// Calling next() is required to finish the middleware task otherwise the paths registered afterwards won't be available
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
@@ -20,16 +23,16 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 // express.static(): need to specify the absolute path of file you want to serve
 // __dirname is an accessible variable that references the root absolute path, D://Documents/nodejs-udemy-Andrew/node-web-server
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerPartials(__dirname + '/views/partials');
-// app.set takes two arguments, key you want to set, value is what you want to use.. eg: view engine we want to use is 'hbs'.
+
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 });
@@ -58,7 +61,8 @@ app.get('/bad', (req, res) => {
   })
 });
 
+
 // app.listen can take a second argument; a callbackfunction since it takes a little time to start up server
-app.listen(3000, () => {
-  console.log('Server is starting on port 3000');
+app.listen(port, () => {
+  console.log(`Server is starting on port ${port}`);
 });
